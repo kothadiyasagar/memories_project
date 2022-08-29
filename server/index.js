166dcf0b60bd3839e1517b7984576fba7c953bfd
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-08-29 14:50:22 +0530

    server

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import  express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors"
+
+const app = express()
+
+app.use(bodyParser.json({limit:"30mp",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mp",extended:true}))
+app.use(cors())
+const CONNECTION_URL = "MOGODB_URL = mongodb+srv://sagar:sagar123@cluster0.dn7hj5y.mongodb.net/memorirs_Project?retryWrites=true&w=majority"
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
