e1e47fce76bff13e915c6bd61cce1b771f97ee16
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-09-04 16:53:14 +0530

    tesla server

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ dotenv.config()
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-
+app.use('/',(req,res)=>{
+  res.send("API")
+})
 app.use('/posts', postRoutes);
 
 const PORT = process.env.PORT|| 5000;
