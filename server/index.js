bdb25c7ecedb7c7b8038510c182b1e1870603d97
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-09-06 09:09:12 +0530

    j

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-dotenv.config()
+dotenv.config({path:"./.env"})
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
@@ -23,4 +23,4 @@ mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnified
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
- mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
