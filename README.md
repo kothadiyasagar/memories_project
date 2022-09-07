231ca324c5341f06cfcc49e20eb581847094f95e
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-09-07 18:00:12 +0530

    done

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# memories_project
+# memories_social media app
+
+ it is a simple social media app that allows users to post interesting events that happened in their lives.
+
+ ## Features
+
+- Login with Email (JWT) + Google OAuth Authentication
+- Pagination.
+- Comments.
+- Like event.
+- Create Update and Delete  memories.
+- Responsive for all devices.
+- Recommended Posts.
+- search memories.
+
+## Tech Stack
+
+- HTML | CSS | JavaScript | React | MongoDB | NodeJS | Express | JWT
+
+### Libraries Used
+
+- Material Ui
--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import  dotenv from 'dotenv'
 import cors from 'cors';
-
+import userRouter from "./routes/user.js";
+import  dotenv from 'dotenv'
 import postRoutes from './routes/posts.js';
 
 const app = express();
@@ -12,15 +12,16 @@ dotenv.config({path:"./.env"})
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-app.use('/',(req,res)=>{
-  res.send("API")
+app.get("/",(req,res)=>{
+  res.send("API RUNNING")
 })
 app.use('/posts', postRoutes);
+app.use('/user',userRouter)
 
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 5500;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
-// mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false);
