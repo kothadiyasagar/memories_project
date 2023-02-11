2db874110356ed2e0b007f64c668fb5e644e2612
kothadiyasagar <sagarkothadiya98@gmail.com>
2023-02-11 16:20:03 +0530

    upadate

--- client/src/Api/index.js
+++ client/src/Api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memories-socials.herokuapp.com' });
+const API = axios.create({ baseURL: 'http://localhost:5500' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
